79ca9854bf1224159280fe86ebf545a61527e71a
core connect <timmokimathi@gmail.com>
2026-04-15 16:08:24 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,11 +4,11 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  authDomain: "zedo-intl.firebaseapp.com",
+  projectId: "zedo-intl",
+  storageBucket: "zedo-intl.firebasestorage.app",
+  messagingSenderId: "461236618420",
+  appId: "1:461236618420:web:7561951ca52dc97f7403e1" 
 };
 
 const app = initializeApp(firebaseConfig);
